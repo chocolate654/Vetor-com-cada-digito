3e5c08c61058737cf5cb4146a8ba4cac1cc940a0
Geovani brasil <geobrasileoliveira11@gmail.com>
2025-05-10 22:29:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 int main()
 {
-    printf("Hello world!\n");
+       srand(time(NULL));
+    int indice_aleatorio=rand()%100000;
+    printf("%d{",indice_aleatorio);
+    int numero=indice_aleatorio;
+    for(int i=100000;i>=1;i/=10){
+        int digito=(numero/i)%10;
+        printf("%d",digito);
+        printf(",");
+    }
+    printf("}\n");
     return 0;
 }
